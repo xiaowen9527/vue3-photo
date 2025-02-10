4b6e3b2940dad7dfba034630f25b3f5366bd547d
IWEN\iwen <1044803551@qq.com>
2025-02-10 15:54:58 +0800

    init project

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<!-- description: index -->
+<!-- fileName: Index.vue -->
+<!-- author: iwen(1044803551@qq.com) -->
+
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        index
+    </div>
+</template>
+
+<style lang='scss' scoped></style>
