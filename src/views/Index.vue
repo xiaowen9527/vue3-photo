14cdcfab6707056631d8765209ac5ee29672fdac
xiaowen9527 <1044803551@qq.com>
2025-02-18 02:36:38 +0000

    添加demo

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,39 +3,56 @@
 <!-- author: iwen(1044803551@qq.com) -->
 
 <script setup>
-    import axios from 'axios'
+import axios from 'axios'
 
 
-    let listId = ref([])
+let listId = ref([])
 
 
-    const getData = () => {
-        axios.get("/mock/getList").then(res => {
+const getData = () => {
+	axios.get("/mock/getList").then(res => {
 
-            listId.value = res.data
-            console.log(listId.value);
+		listId.value = res.data
+		console.log(listId.value);
 
+		listId.value.forEach(e => {
 
-            // let d = res.data;
-            // if (d.length > 10000) {
-            //     let dd = d.slice(
-            //         d.indexOf("window.__INITIAL_STATE__=") + 25,
-            //         -23
-            //     );
-            //     let json = eval("(" + dd + ")");
-            //     console.log(json.user.notes[0]);
-            // }
-        }).catch(err => {
-            console.log(err);
-        })
-    }
-    getData()
+			axios.get('/xhs/user/profile/' + e)
+				.then(res => {
+					let d = res.data;
+					if (d.length > 10000) {
+						let dd = d.slice(
+							d.indexOf("window.__INITIAL_STATE__=") + 25,
+							-23
+						);
+						let json = eval("(" + dd + ")");
+						console.log(json.user.notes[0]);
+					}
+				}).catch(err => {
+					console.log(err);
+				})
+		});
+
+		// let d = res.data;
+		// if (d.length > 10000) {
+		//     let dd = d.slice(
+		//         d.indexOf("window.__INITIAL_STATE__=") + 25,
+		//         -23
+		//     );
+		//     let json = eval("(" + dd + ")");
+		//     console.log(json.user.notes[0]);
+		// }
+	}).catch(err => {
+		console.log(err);
+	})
+}
+getData()
 </script>
 
 <template>
-    <div>
-        index
-    </div>
+	<div>
+		index
+	</div>
 </template>
 
 <style lang='scss' scoped></style>
